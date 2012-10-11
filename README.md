03b1e265241b5aefc5e9602a74f407c7a8856b14
Christian Polzer <c.polzer@roothausen.de>
2012-10-11 06:58:18 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,54 @@
 jQDrawBootstrapGrid
 ===================
 
-A simple jQuery plugin that draws grid columns to a twitter boortstrap enabled layout. 
+A simple jQuery plugin that draws grid columns to a twitter bootstrap layout. 
+Note: By default automatically add a "show/hide grid" button and start in hidden mode.
+
+Requirements
+------------
+Twitter Bootstrap
+JQuery < 1.7.*
+
+Usage
+-----
+ After adding the javascript source to your document,
+ apply the plugin to the bootstrap ".container":
+ jQuery(".container").drawBootstrapGrid();
+
+Options
+-------
+The default settings are:
+{
+    'columns':12,   //define how much columns to draw
+     'singleColumnName': 'span1', //the css class name used in your bootstrap (just being caucious)
+    'color':'lightgrey', //each columns background color
+    'opacity':0.3,  //opacity of the rendered grid
+    'buttonLabel': 'Show/Hide Grid', //the label for the button
+    'startHidden':true, //if we want the grid to be shown initially
+    'hiddenClassName': 'hidden' //the css class name used in your bootstrap to hide elements -> visibility: hidden
+}
+
+ 
+Support
+-------
+
+Have a question, or found an issue? Just create a issue: https://github.com/plozi/jQDrawBootstrapGrid/issues
+
+
+Authors
+-------
+
+**Christian Polzer**
+
++ chris@hai-fai.de
++ http://twitter.com/polzifer
++ http://github.com/plozi
++ http://hai-fai.de
+
+
+Copyright and License
+---------------------
+
+
+The jQDrawBootstrapGrid Plugin is dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) 
+and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses. Copyright (c)2012 Christian Polzer.
---
+++ src/jqdrawbsgrid.js
@@ -0,0 +1,78 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: polzer
+ * Date: 11.10.12
+ * Time: 11:17
+ *
+ * Description:
+ * A simple jQuery plugin to draw single grid lines.
+ * Usually applied to the bootstrap "container":
+ * jQuery(".container").drawBootstrapGrid();
+ *
+ * Default settings:
+ *  {
+ *    'columns':12,   //define how much columns to draw
+ *     'singleColumnName': 'span1', //the css class name used in your bootstrap (just being caucious)
+ *    'color':'lightgrey', //each columns background color
+ *    'opacity':0.3,  //opacity of the rendered grid
+ *    'buttonLabel': 'Show/Hide Grid', //the label for the button
+ *    'startHidden':true, //if we want the grid to be shown initially
+ *    'hiddenClassName': 'hidden' //the css class name used in your bootstrap to hide elements -> visibility: hidden
+ *   }
+ *
+ * Fork me at github: https://github.com/plozi/jQDrawBootstrapGrid
+ */
+ 
+;(function ($) {
+
+    $.fn.drawBootstrapGrid = function (options) {
+
+        // Create some defaults, extending them with any options that were provided
+        var settings = $.extend({
+            'columns':12,
+            'singleColumnName': 'span1',
+            'color':'lightgrey',
+            'opacity':0.3,
+            'buttonLabel': 'Show/Hide Grid',
+            'startHidden': true,
+            'hiddenClassName': 'hidden'
+        }, options)
+
+        return this.each(function () {
+            var $this = jQuery(this),
+                i = 0,
+                height = $this.innerHeight() + 'px',
+                $gridEl = jQuery('<div></div>').addClass('grid').css("position", "absolute")
+                if(settings.startHidden){
+                    $gridEl.addClass(settings.hiddenClassName);
+                }
+                $showHideButton = jQuery('<button></button>')
+                    .addClass("btn btn-primary")
+                    .css('position','fixed')
+                    .css('top', '0')
+                    .css('right', '0')
+                    .css('margin','10px')
+                    .text(settings.buttonLabel)
+                    .click(function(){
+                        jQuery($gridEl).toggleClass(settings.hiddenClassName);
+                    })
+
+            $this.append($gridEl)
+            $this.append($showHideButton)
+            while (i < settings.columns) {
+                $gridEl.append(
+                    jQuery('<div></div>')
+                        .addClass(settings.singleColumnName)
+                        .css('background', settings.color)
+                        .css('opacity', settings.opacity)
+                        .css('border-radius', '2px')
+                        .css('height', height)
+                        .css('z-index', '-2')
+                )
+                i++
+            }
+        });
+
+    };
+
+})(jQuery);
